d36314341d7c6364319c7bb64e47283144cf7117
amitkushwaha002 <96170636+amitkushwaha002@users.noreply.github.com>
2025-01-19 17:11:16 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,18 @@
+const accountID = 144553
+let accountEmail = "amit@gmail.com"
+var accountPassword = "12345" //prefer not to use var
+accountCity="jaipur"//not a good idea to initialize a variable
+let accountState
+// accountID=2
+accountEmail="hc@hc.com"
+accountPassword= "2121"
+accountCity="delhi"
+
+/*
+console.log(accountID);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+*/
+
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState]);
