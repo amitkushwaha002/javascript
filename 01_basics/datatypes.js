d1b14874dad570f39bcfb6894d24b996759344ee
amitkushwaha002 <96170636+amitkushwaha002@users.noreply.github.com>
2025-01-19 17:33:41 +0000

    datatypes

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -1,1 +1,7 @@
 "use strict" // treat all java script code as a newer version
+//alert(3+3) we are using nodejs not a browser
+console.log(3+3);
+console.log("Amit")
+let name="Amit"
+let age=18
+let isloggedIN=false
